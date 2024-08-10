12f5b888a6ff2cb2a18bb5b7ee5a71981c6e357b
Pedro Adorno <pedroadorno99@gmail.com>
2024-08-09 22:56:00 -0300

    fix: removes gitignore and readds waybar to hypr startup

--- hosts/workmachine/modules/home-manager/wms/hypr/startup.nix
+++ hosts/workmachine/modules/home-manager/wms/hypr/startup.nix
@@ -3,7 +3,7 @@
     exec-once = [
       ''dconf write /org/gnome/desktop/interface/cursor-theme "'Bibata-Modern-Ice'"''
       "hyprctl setcursor Bibata-modern 20"
-      # "waybar"
+      "waybar"
     ];
   };
 }
