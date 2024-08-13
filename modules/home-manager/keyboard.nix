9142a5f92a4c99c7811de29579dfc3ae38223b74
Pedro Adorno <pedroadorno99@gmail.com>
2024-08-13 00:09:56 -0300

    adds diaresis to quotedbl

--- modules/home-manager/keyboard.nix
+++ modules/home-manager/keyboard.nix
@@ -4,5 +4,8 @@
 
     <dead_acute> <C> : "Ç"
     <dead_acute> <c> : "ç"
+    <dead_diaresis> <dead_diaresis> : "\""
+    <Multi_key> <quotedbl> <space> : "\""
+    <Multi_key> <quotedbl> <quotedbl> : "\""
   '';
 }
