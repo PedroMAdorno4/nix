a8a9450ece93b45a4c334e837da226dcc985bf5d
Pedro Adorno <pedroadorno99@gmail.com>
2024-08-12 23:48:02 -0300

    feat: adds fcitx5 config to nix

--- modules/nixos/modules/i18n.nix
+++ modules/nixos/modules/i18n.nix
@@ -12,12 +12,69 @@
             fcitx5-gtk
             fcitx5-nord
           ];
+
+          settings = {
+            globalOptions = {
+              "Hotkey" = {
+                "EnumerateWithTriggerKeys" = true;
+                "EnumerateForwardKeys" = "";
+                "EnumerateBackwardKeys" = "";
+                "EnumerateSkipFirst" = false;
+                "EnumerateGroupForwardKeys" = "";
+                "EnumerateGroupBackwardKeys" = "";
+                "ActivateKeys" = "";
+                "DeactivateKeys" = "";
+                "PrevPage" = "";
+                "NextPage" = "";
+                "PrevCandidate" = "";
+                "NextCandidate" = "";
+                "TogglePreedit" = "";
+              };
+              "Hotkey/TriggerKeys" = {
+                "0" = "Control+space";
+              };
+              "Behavior" = {
+                "ActiveByDefault" = false;
+                "ResetStateWhenFocusIn" = "No";
+                "ShareInputState" = "No";
+                "PreeditEnabledByDefault" = true;
+                "ShowInputMethodInformation" = true;
+                "ShowInputMethodInformationWhenFocusIn" = false;
+                "CompactInputMethodInformation" = true;
+                "ShowFirstInputMethodInformation" = true;
+                "DefaultPageSize" = 5;
+                "OverrideXkbOption" = false;
+                "CustomXkbOption" = "";
+                "EnabledAddons" = "";
+                "DisabledAddons" = "";
+                "PreloadInputMethod" = true;
+                "AllowInputMethodForPassword" = false;
+                "ShowPreeditForPassword" = false;
+                "AutoSavePeriod" = 30;
+              };
+            };
+            inputMethod = {
+              "Groups/0" = {
+                "Name" = "Default";
+                "Layout" = "us";
+                "DefaultIM" = "keyboard-us-intl";
+              };
+              "Groups/0/Items/0" = {
+                "Name" = "keyboard-us-intl";
+                "Layout" = "";
+              };
+              "GroupOrder" = {
+                "0" = "Default";
+              };
+            };
+          };
         };
       };
 
     extraLocaleSettings = {
       LC_TIME = "pt_BR.UTF-8";
     };
+
     supportedLocales = [ "all" ];
   };
 }
