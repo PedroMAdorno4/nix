012550af2634f265afca0e03267872737331c4a8
Pedro Adorno <pedroadorno99@gmail.com>
2024-08-16 16:57:28 -0300

    chore: bump versions

--- modules/nixos/modules/sudo.nix
+++ modules/nixos/modules/sudo.nix
@@ -1,5 +1,43 @@
-{
+{ pkgs, ... }: {
   security.sudo = {
     enable = true;
+    extraRules = [{
+      commands = [
+        {
+          command = "${pkgs.systemd}/bin/systemctl";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "${pkgs.systemd}/bin/reboot";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "${pkgs.systemd}/bin/poweroff";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "${pkgs.systemd}/bin/systemd-run";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "${pkgs.nh}/bin/nh";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "${pkgs.nixos-rebuild}/bin/nixos-rebuild";
+          options = [ "NOPASSWD" ];
+        }
+      ];
+      groups = [ "wheel" ];
+    }];
+    extraConfig = with pkgs; ''
+      Defaults:quatro secure_path="${lib.makeBinPath [
+        systemd
+      ]}:${lib.makeBinPath [
+        nh
+      ]}:${lib.makeBinPath [
+        nixos-rebuild
+      ]}:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
+    '';
   };
 }
