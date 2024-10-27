e434ff3a537c29f9d9ddd6e37590299b8bf803f5
Pedro Adorno <pedro.adorno@liven.tech>
2024-10-27 14:18:52 -0300

    adds lockscreen wallpaper

--- modules/home-manager/hyprlock.nix
+++ modules/home-manager/hyprlock.nix
@@ -1,4 +1,4 @@
-{ pkgs, ...}: {
+{ pkgs, config, ... }: {
   programs.hyprlock = {
     enable = true;
     settings = {
@@ -11,7 +11,7 @@
 
       background = [
         {
-          path = "../../wallpaper.webp";
+          path = "${config.home.homeDirectory}/nix/lockscreen.jpg";
           blur_passes = 1;
           blur_size = 3;
         }
@@ -20,8 +20,8 @@
       input-field = [
         {
           size = "200, 50";
-          position = "0, -80";
-          monitor = "DP-3";
+          position = "0, -450";
+          monitor = "";
           dots_center = true;
           fade_on_empty = false;
           font_color = "rgb(202, 211, 245)";
@@ -30,6 +30,18 @@
           outline_thickness = 5;
           placeholder_text = ''<span foreground="##cad3f5">Password...</span>'';
           shadow_passes = 2;
+        }
+      ];
+
+      label = [
+        {
+          monitor = "";
+          text = "$TIME";
+          text_align = "center";
+          color = "rgba(200, 200, 200, 1.0)";
+          font_size = 25;
+          font_family = "Noto Sans";
+          position = "60, 60";
         }
       ];
     };
