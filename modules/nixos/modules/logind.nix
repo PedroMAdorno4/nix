4f94a3e728d8b1a810f7077d238c0238f1d60f86
Pedro Adorno <pedro.adorno@liven.tech>
2025-05-02 12:38:55 -0300

    refactor(workmachine): logind disables lid switch

--- modules/nixos/modules/logind.nix
+++ modules/nixos/modules/logind.nix
@@ -1,6 +1,3 @@
 {
-  services.logind.extraConfig = ''
-    # want to be able to listen to music while laptop closed
-    LidSwitchIgnoreInhibited=no
-  '';
+  services.logind.lidSwitch = "ignore";
 }
